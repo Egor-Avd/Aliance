f363947c47747c2d5521a9cde27f44f0bde2340a
Egor Avdeev <Avdeev.eg@icloud.com>
2025-10-24 08:38:56 +0500

    fix: script modal-dialog

--- js/main.js
+++ js/main.js
@@ -8,19 +8,21 @@ const lightModeOn = (event) => {
   navbar.classList.add("navbar-light");
   logo.style.display = "block";
   logoLight.style.display = "none";
-}
+};
 const lightModeOff = (event) => {
   navbar.classList.remove("navbar-light");
   logo.style.display = "none";
   logoLight.style.display = "block";
-}
-const openMenu = (event) => { // функция открывания меню
+};
+const openMenu = (event) => {
+  // функция открывания меню
   menu.classList.add("is-open"); // вешает класс "is-open"
   mMenuToggle.classList.add("close-menu");
   document.body.style.overflow = "hidden"; // запрещаем прокрутку сайта под меню
   lightModeOn();
 };
-const closeMenu = (event) => { // функция закрывания меню
+const closeMenu = (event) => {
+  // функция закрывания меню
   menu.classList.remove("is-open"); // удаляет класс "is-open"
   mMenuToggle.classList.remove("close-menu");
   document.body.style.overflow = ""; // возвращает прокрутку сайта
@@ -28,7 +30,6 @@ const closeMenu = (event) => { // функция закрывания меню
 };
 window.addEventListener("scroll", () => {
   this.scrollY > 1 ? lightModeOn() : lightModeOff();
-  
 });
 mMenuToggle.addEventListener("click", (event) => {
   event.preventDefault();
@@ -39,9 +40,9 @@ const swiperSteps = new Swiper(".steps-slider", {
   speed: 400,
   slidesPerView: 1,
   navigation: {
-      nextEl: '.steps-button-next',
-      prevEl: '.steps-button-prev',
-    },
+    nextEl: ".steps-button-next",
+    prevEl: ".steps-button-prev",
+  },
   breakpoints: {
     // Окно >=576 px
     576: {
@@ -55,57 +56,62 @@ const swiperSteps = new Swiper(".steps-slider", {
     1024: {
       slidesPerView: 4,
     },
-    },
+  },
 });
 
-  const swiper = new Swiper('.features-slider', {
-    speed: 400,
-    slidesPerView: 1,
-    navigation: {
-      nextEl: '.slider-button-next',
-      prevEl: '.slider-button-prev',
+const swiper = new Swiper(".features-slider", {
+  speed: 400,
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev",
+  },
+  breakpoints: {
+    // Окно >=576 px
+    576: {
+      slidesPerView: 2,
     },
-    breakpoints: {
-      // Окно >=576 px
-      576: {
-        slidesPerView: 2,
-        },
-      // Окно >=768 px
-      768: {
-        slidesPerView: 3,
-        },
-      // Окно >=1200 px
-      1024: {
-        slidesPerView: 4,
-        },
-      // Окно >1200 px
-      1200: {
-        slidesPerView: 5,
-        },
+    // Окно >=768 px
+    768: {
+      slidesPerView: 3,
     },
-  });
+    // Окно >=1200 px
+    1024: {
+      slidesPerView: 4,
+    },
+    // Окно >1200 px
+    1200: {
+      slidesPerView: 5,
+    },
+  },
+});
 
 const swiperBlog = new Swiper(".blog-slider", {
   speed: 400,
   spaceBetween: 30,
   slidesPerView: 2,
   navigation: {
-    nextEl: '.blog-button-next',
-    prevEl: '.blog-button-prev',
-  }
+    nextEl: ".blog-button-next",
+    prevEl: ".blog-button-prev",
+  },
 });
 
-const modal = document.querySelector('.modal');
-const modalToggle = document.querySelectorAll('[data-toggle=modal]');
-const modalClose = document.querySelector('.modal-close');
-console.log(modalToggle);
-modalToggle.forEach((Element) => {
-  Element.addEventListener('click', (event) => {
+const modal = document.querySelector(".modal");
+const modalDialog = document.querySelector(".modal-dialog");
+
+document.addEventListener("click", (event) => {
+  if (
+    event.target.dataset.toggle == "modal" ||
+    event.target.parentNode.dataset.toggle == "modal" ||
+    (!event.composedPath().includes(modalDialog) &&
+      modal.classList.contains("is-open"))
+  ) {
     event.preventDefault();
-    modal.classList.add('is-open');
-  });
+    modal.classList.toggle("is-open");
+  }
 });
-modalClose.addEventListener('click', (event) => {
-  event.preventDefault();
-  modal.classList.remove('is-open');
+document.addEventListener("keyup", (event) => {
+  if (event.key == "Escape" && modal.classList.contains("is-open")) {
+    modal.classList.toggle("is-open");
+  }
 });
